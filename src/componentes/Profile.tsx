ce8cb44f16dad6bc20999fa16ac1ec0812e5a3e8
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-09-15 14:30:07 -0300

    style: Profile Component

--- src/componentes/Profile.tsx
+++ src/componentes/Profile.tsx
@@ -1,10 +1,15 @@
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
     return (
-        <div>
+        <div className={styles.profileContainer}>
             <img src="https://github.com/fabiomoura-m.png" alt="" />
             <div>
                 <strong>Fabio Moura</strong>
-                <p>Level 1</p>
+                <p>
+                    <img src="icons/level.svg" alt="" />
+                    Level 1
+                </p>
             </div>
         </div>
     );
