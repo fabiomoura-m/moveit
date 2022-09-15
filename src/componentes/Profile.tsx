7742a1c4e60b4fc292a44bdba36695202d8c7966
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-09-15 14:24:32 -0300

    feat: add Profile Component

---
+++ src/componentes/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/fabiomoura-m.png" alt="" />
+            <div>
+                <strong>Fabio Moura</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from 'next';
 import { ExperienceBar } from '../componentes/ExperienceBar';
+import { Profile } from '../componentes/Profile';
 
 const Home: NextPage = () => {
     return (
         <div className="container">
             <ExperienceBar />
+            <section>
+                <div>
+                    <Profile />
+                </div>
+                <div></div>
+            </section>
         </div>
     );
 };
